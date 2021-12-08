18f546fc1df62cb5a243dbcc219a9f0826e137ef
totetmatt <matthieu.totet@gmail.com>
2021-12-08 19:34:28 +0100

    Better day05

--- src/main/scala/Day05.scala
+++ src/main/scala/Day05.scala
@@ -19,36 +19,17 @@ object Day05 extends App {
   }
   def gen(from: Vec2, to: Vec2) = {
 
-    if (from.x == to.x) {
-
-      Some(
-        Math
-          .min(from.y, to.y)
-          .to(Math.max(from.y, to.y))
-          .map(i => Vec2(from.x, i))
-      )
-    } else if (from.y == to.y) {
-      Some(
-        Math
-          .min(from.x, to.x)
-          .to(Math.max(from.x, to.x))
-          .map(i => Vec2(i, from.y))
-      )
-    } else {
-
-      Some(
-        Iterator.from(
-          gengen(
-            from,
-            to,
-            Vec2(
-              Math.signum(to.x - from.x).toInt,
-              Math.signum(to.y - from.y).toInt
-            )
-          )
+    Iterator.from(
+      gengen(
+        from,
+        to,
+        Vec2(
+          Math.signum(to.x - from.x).toInt,
+          Math.signum(to.y - from.y).toInt
         )
       )
-    }
+    )
+
   }
   val result = data
     .flatMap(line => {
@@ -60,7 +41,6 @@ object Day05 extends App {
         Vec2(toX, toY)
       )
     })
-    .flatten
     .groupBy(identity)
     .count(_._2.size > 1)
   println(result)
